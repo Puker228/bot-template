096ef1ae46d3200ea399e386f2ccf388fc3c061d
SWAGer <131869327+Puker228@users.noreply.github.com>
2024-05-28 19:51:00 +0300

    some buttons

--- app/admin_handlers.py
+++ app/admin_handlers.py
@@ -1,18 +1,27 @@
-from aiogram import Router, F
+from aiogram import Router, F, html
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from aiogram.enums import ParseMode
 
 from config import load_config
 from other.texts import admin_lexicon
 from database.database import create_table
+from .kb import create_keyboard
+from .filters.admin_check import AdminIdsFilter
 
 admin_router = Router()
 config = load_config()
 
+admin_router.message.filter(AdminIdsFilter(config.tg_bot.admin_ids))
+
 
 @admin_router.message(CommandStart())
 async def start_message(message: Message):
-    await message.answer(text=admin_lexicon['admin_start'])
+    await message.answer(
+        text=f"Hello, {html.bold(message.from_user.full_name)}! You are admin",
+        parse_mode=ParseMode.HTML,
+        reply_markup=create_keyboard("button 1", "button 2")
+    )
 
 
 @admin_router.message(Command(commands='table'))
--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,7 @@
-from aiogram import Router, F
+from aiogram import Router, F, html
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
+from aiogram.enums import ParseMode
 
 from config import load_config
 from other.texts import lexicon
@@ -12,9 +13,18 @@ config = load_config()
 
 @router.message(CommandStart())
 async def start_message(message: Message):
-    await message.answer(text=lexicon['/start'], reply_markup=create_keyboard("button 1", "button 2"))
+    await message.answer(
+        text=f"Hello, {html.bold(message.from_user.full_name)}!",
+        parse_mode=ParseMode.HTML,
+        reply_markup=create_keyboard("button 1", "button 2")
+    )
 
 
 @router.message(Command(commands='remove'))
 async def remove_kb(message: Message):
     await message.answer(text='229', reply_markup=ReplyKeyboardRemove())
+
+
+@router.callback_query()
+async def button_text(callback: CallbackQuery):
+    await callback.message.answer(text=f"{callback.data}, puks")
